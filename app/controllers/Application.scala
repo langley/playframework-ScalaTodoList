0ecdd8464ba9ae9bc60ea7dcd43add8b2f994a94
John Langley <digerat@gmail.com>
2013-09-24 13:20:47 -0400

    step-3: Added a definiton of the Task model and updated the   index.scala.html view and definition of Application.tasks to use   it.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,14 +2,24 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+
+import models.Task
 
 object Application extends Controller {
 
+  val taskForm = Form(
+	  "label" -> nonEmptyText
+  )  
+  
   def index = Action {
 	  Redirect(routes.Application.tasks)
   }
-  
-  def tasks = TODO
+
+  def tasks = Action {
+	  Ok(views.html.index(Task.all(), taskForm))
+  }
 
   def newTask = TODO
 
---
+++ app/models/Task.scala
@@ -0,0 +1,13 @@
+package models
+
+case class Task(id: Long, label: String)
+
+object Task {
+  
+  def all(): List[Task] = Nil
+  
+  def create(label: String) {}
+  
+  def delete(id: Long) {}
+  
+}
