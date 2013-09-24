ff87604a6f54b762fed1c717f1ece2b1bc757a0f
John Langley <digerat@gmail.com>
2013-09-24 13:24:59 -0400

    step-4: Added support for form submission of new tasks. NOTE: it does   not persist them yet.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -21,7 +21,15 @@ object Application extends Controller {
 	  Ok(views.html.index(Task.all(), taskForm))
   }
 
-  def newTask = TODO
+  def newTask = Action { implicit request =>
+  	taskForm.bindFromRequest.fold(
+  			errors => BadRequest(views.html.index(Task.all(), errors)),
+  			label => {
+  				Task.create(label)
+  				Redirect(routes.Application.tasks)
+  			}
+  	)
+  }
 
   def deleteTask(id: Long) = TODO
   
